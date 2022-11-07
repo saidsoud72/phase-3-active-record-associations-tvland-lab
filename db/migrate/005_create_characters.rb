7cce0206423e3976031d99990fadb10d2e366e9e
saidsoud72 <said.omar@student.moringaschool.com>
2022-11-07 08:56:47 +0300

    done

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id, foreign_key: true
+      t.integer :show_id, foreign_key: true
+    end
   end
 end
